4220ebd315bb751284be60b8f0761702c7972e8f
Bernabe <bernatattoo@gmail.com>
2023-05-15 12:30:34 -0300

    add firebase firestore

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,32 @@
+import { useState, useEffect } from "react";
 import SignOut from "./SignOut";
-// import { useAuthState } from "react-firebase-hooks/auth";
-// import { auth } from "../firebase/firebaseConfig";
+import { db } from "../firebase/firebaseConfig";
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
 
 const Chat = () => {
-  //const [user] = useAuthState(auth);
+  const [message, setMessage] = useState([]);
+  useEffect(() => {
+    const newQuery = query(collection(db, "messages"), orderBy("fecha"));
+    const unsuscribe = onSnapshot(newQuery, (querysnapshot) => {
+      let currentMessage = [];
+      querysnapshot.forEach((doc) => {
+        currentMessage.push({ content: doc.data(), id: doc.id });
+      });
+      setMessage(currentMessage);
+    });
+    return unsuscribe;
+  });
+
+  console.log(message);
 
   return (
     <div>
       <h3>Chat App</h3>
+      {message.map((item) => (
+        <div key={item.id}>
+          <p>{item.content.text}</p>
+        </div>
+      ))}
       <SignOut />
     </div>
   );
