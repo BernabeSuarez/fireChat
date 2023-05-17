790cda8b72a0ce8226f5f9502140b8ebc8d82198
Bernabe <bernatattoo@gmail.com>
2023-05-17 00:56:56 -0300

    remove PWA

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    manifest: {
-      icons: [
-        {
-          src: '/favicon.ico',
-          sizes: '64x64',
-          type: 'image/x-icon',
-          purpose: "any maskable"
-        }
-      ]
-    }
-  })],
+  plugins: [react(),
+  ]
 })
+
