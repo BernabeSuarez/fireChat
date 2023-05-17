d0532e824514a8fb67c175996239c63b9425feb1
Bernabe <bernatattoo@gmail.com>
2023-05-17 00:49:44 -0300

    Modifi Manifest

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA({
+    manifest: {
+      icons: [
+        {
+          src: '/favicon.ico',
+          sizes: '64x64',
+          type: 'image/x-icon',
+          purpose: "any maskable"
+        }
+      ]
+    }
+  })],
 })
